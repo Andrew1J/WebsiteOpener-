afff171af56237ba544181cb9bb08f84cc898fe7
Andrew1J <61942832+Andrew1J@users.noreply.github.com>
2020-06-25 13:16:04 -0400

    Update verycoolprogram.py

--- verycoolprogram.py
+++ verycoolprogram.py
@@ -3,7 +3,7 @@ import time
 import random
 
 while True:
-    bob = random.choice(['google.com', 'www.youtube.com/watch?v=dQw4w9WgXcQ', 'homer.stuy.edu/~ali20/', 'weather.gov', 'theuselessweb.com/'])
+    bob = random.choice(['google.com', 'www.youtube.com/watch?v=dQw4w9WgXcQ', 'moe.stuy.edu/~ajuang20/cwhw40.html', 'weather.gov', 'theuselessweb.com/'])
     billyboi = 'http://{}'.format(bob)
     webbrowser.open(billyboi)
     seconds = random.randrange(5, 20)
